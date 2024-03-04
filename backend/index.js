f40dfaa38d226e24b4483960ce256c7b907b2949
Dee-Codez <debampati44@gmail.com>
2024-03-05 01:07:36 +0530

    DBAuthMiddleware

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,12 @@
 const express = require("express");
+var cors = require('cors')
+const app = express();
 
+app.use(cors());
+app.use(express.json());
 
+var mainRoutes = require('./routes/index');
+
+app.use('/api/v1', mainRoutes);
+
+app.listen(3000);
