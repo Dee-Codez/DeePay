177bbd75721fefd4017390be3f01b425a93129e6
Dee-Codez <debampati44@gmail.com>
2024-03-05 22:05:38 +0530

    Final_1

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 var cors = require('cors')
+const bodyParser = require("body-parser")
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 var mainRoutes = require('./routes/index');
 
